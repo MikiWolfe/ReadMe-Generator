a1cd02b269a735fa35484b85015dbced55265a6f
Mikayla Bruce <mkla.w.b@gmail.com>
2022-01-05 18:19:17 -0800

    started along with instructor demo

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 // TODO: Include packages needed for this application
-
+const inquirer =require('inquirer');
+const markdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        name: "title",
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt(questions)
+    .then(answers =>{
+        console.log(answers);
+        const myMarkdown = markdown( answers);
+        // call the write to file function with file name and myMarkdown
+    })
+}
 
 // Function call to initialize app
 init();
