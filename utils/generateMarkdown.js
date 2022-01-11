6a1623fe1ff2ba2d9d3d4e317eaf17cfaf7cc763
Mikayla Bruce <mkla.w.b@gmail.com>
2022-01-11 13:20:34 -0800

    commented back in questions that were commented out for testing

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -60,7 +60,7 @@ const apacheURL = "https://opensource.org/licenses/Apache-2.0";
 const gplURL = "https://www.gnu.org/licenses/gpl-3.0";
 const bsdURL = "https://opensource.org/licenses/BSD-2-Clause";
 
-const mitBlurb = `Copyright [yyyy] [name of copyright owner] Permission is hereby granted, free of charge, 
+const mitBlurb = `Copyright Permission is hereby granted, free of charge, 
 to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in 
 the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, 
 distribute, sublicense, and/or sell 
@@ -75,7 +75,7 @@ THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRI
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
 TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
 SOFTWARE.`
-const apacheBlurb = `Copyright [yyyy] [name of copyright owner] Licensed under the Apache License, Version 2.0 
+const apacheBlurb = `Copyright Licensed under the Apache License, Version 2.0 
 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -87,14 +87,14 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.`;
-const gplBlurb = `Copyright [yyyy] [name of copyright owner] This program is free software: you can redistribute it 
+const gplBlurb = `This program is free software: you can redistribute it 
 and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, 
 either version 3 of the License, or (at your option) any later version. This program is distributed in the hope 
 that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
 FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of 
 the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.`;
 
-const bsdBlurb = `Copyright [yyyy] [name of copyright owner] Redistribution and use in source and binary forms, 
+const bsdBlurb = ` Redistribution and use in source and binary forms, 
 with or without modification, are permitted provided that the following conditions are met:
 
 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
